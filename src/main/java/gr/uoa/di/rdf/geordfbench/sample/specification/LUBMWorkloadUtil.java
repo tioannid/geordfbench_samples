fea4ab174da113e27b050848344ee93be489f3fd
Theofilos Ioannidis <tioannid@yahoo.com>
2024-08-02 12:58:01 +0300

    Added more LUBM queries and formated them better

--- src/main/java/gr/uoa/di/rdf/geordfbench/sample/specification/LUBMWorkloadUtil.java
+++ src/main/java/gr/uoa/di/rdf/geordfbench/sample/specification/LUBMWorkloadUtil.java
@@ -1,4 +1,4 @@
-package gr.uoa.di.rdf.geordfbench.createlubmworkload;
+package gr.uoa.di.rdf.geordfbench.sample.specification;
 
 import com.fasterxml.jackson.databind.JsonMappingException;
 import gr.uoa.di.rdf.Geographica3.runtime.datasets.complex.impl.GeographicaDS;
@@ -17,11 +17,11 @@ import org.apache.log4j.Logger;
 import org.eclipse.rdf4j.rio.RDFFormat;
 import java.nio.file.Paths;
 
-public class WorkloadUtil {
+public class LUBMWorkloadUtil {
 
     // --- Static members -----------------------------
     static Logger logger
-            = Logger.getLogger(WorkloadUtil.class.getSimpleName());
+            = Logger.getLogger(LUBMWorkloadUtil.class.getSimpleName());
     static final String LUBM_1_0_JSONDEF_FILE = "LUBM_1_0_WLoriginal_GOLD_STANDARD.json";
     static final String NTRIPLES_STR = RDFFormat.NTRIPLES.getName().toUpperCase();
 
@@ -65,28 +65,60 @@ public class WorkloadUtil {
         Map<Integer, IQuery> mapQry = new HashMap<>();
         // using SimpleQuery(String label, String query, boolean usePredicate, long expectedResults)
         mapQry.put(0, new SimpleQuery("Q1_GradStudents_Taken_GradCource0_At_Univ0",
-                "SELECT ?x WHERE { \n ?x rdf:type ub:GraduateStudent . \n ?x ub:takesCourse <http://www.Department0.University0.edu/GraduateCourse0> .\n} \n",
+                "SELECT ?x WHERE {\n"
+                + " ?x rdf:type ub:GraduateStudent .\n"
+                + " ?x ub:takesCourse <http://www.Department0.University0.edu/GraduateCourse0> .\n"
+                + "} \n",
                 false,
                 4));
         mapQry.put(1, new SimpleQuery("Q2_GradStudents_From_Any_Univ_Depart",
-                "SELECT ?x ?y ?z WHERE { \n ?x rdf:type ub:GraduateStudent .\n"
+                "SELECT ?x ?y ?z WHERE {\n"
+                + " ?x rdf:type ub:GraduateStudent .\n"
                 + " ?y rdf:type ub:University .\n"
                 + " ?z rdf:type ub:Department .\n"
                 + " ?x ub:memberOf ?z .\n"
                 + " ?z ub:subOrganizationOf ?y .\n"
-                + " ?x ub:undergraduateDegreeFrom ?y .\n} \n",
+                + " ?x ub:undergraduateDegreeFrom ?y .\n"
+                + "} \n",
                 false,
                 0));
         mapQry.put(2, new SimpleQuery("Q3_Publications_Of_AssistProfessor0_In_Univ0_Depart0",
                 "SELECT ?x WHERE { \n ?x rdf:type ub:Publication .\n"
-                + " ?x ub:publicationAuthor <http://www.Department0.University0.edu/AssistantProfessor0> .\n} \n",
+                + " ?x ub:publicationAuthor <http://www.Department0.University0.edu/AssistantProfessor0> .\n"
+                + "} \n",
                 false,
                 6));
-        mapQry.put(3, new SimpleQuery("Q14_UndergradStudents_From_Any_Univ_Depart",
-                "SELECT ?x WHERE { \n ?x rdf:type ub:UndergraduateStudent .\n} \n",
+        mapQry.put(3, new SimpleQuery("Q4_Properties_Of_Professors_In_Univ0_Depart0",
+                "SELECT ?x ?y1 ?y2 ?y3 WHERE {\n"
+                + "  ?x rdf:type ub:Professor .\n"
+                + "  ?x ub:worksFor <http://www.Department0.University0.edu> .\n"
+                + "  ?x ub:name ?y1 .\n"
+                + "  ?x ub:emailAddress ?y2 .\n"
+                + "  ?x ub:telephone ?y3 .\n"
+                + "} \n",
+                false,
+                -1));
+        mapQry.put(4, new SimpleQuery("Q5_Persons_MembersOf_In_Univ0_Depart0",
+                "SELECT ?x WHERE {\n"
+                + "  ?x rdf:type ub:Person .\n"
+                + "  ?x ub:memberOf <http://www.Department0.University0.edu> .\n"
+                + "} \n",
+                false,
+                -1));
+        mapQry.put(5, new SimpleQuery("Q6_All_Students",
+                "SELECT ?x WHERE {\n"
+                + "  ?x rdf:type ub:Student .\n"
+                + "} \n",
+                false,
+                -1));
+        // ... add as many queries as desired
+        mapQry.put(13, new SimpleQuery("Q14_UndergradStudents_From_Any_Univ_Depart",
+                "SELECT ?x WHERE {\n"
+                + "  ?x rdf:type ub:UndergraduateStudent .\n"
+                + "} \n",
                 false,
                 5916));
-
+        
         StaticTempParamQS lubm_qs = new StaticTempParamQS(SimpleES.newMicroES(), "lubm-1_0", "", false, mapQry,
                 mapTemplateParams, mapUsefulNamespacePrefixes,
                 mapLiteralValues);
@@ -94,7 +126,6 @@ public class WorkloadUtil {
                 lubm_ds,
                 lubm_qs);
         try {
-
             swl.serializeToJSON(Paths.get(outputDir, LUBM_1_0_JSONDEF_FILE).toFile());
         } catch (JsonMappingException ex) {
             logger.error(ex.getMessage());
@@ -108,13 +139,13 @@ public class WorkloadUtil {
         if (args.length != 1) {
             logger.info("Arguments not correct");
             logger.info("Arguments: " + args);
-            logger.info("Usage: " + WorkloadUtil.class.getSimpleName() + " <JSON spec output path>");
+            logger.info("Usage: " + LUBMWorkloadUtil.class.getSimpleName() + " <JSON spec output path>");
             System.exit(-1);
         }
         String outputDir = args[0];
         String outputFile = Paths.get(outputDir, LUBM_1_0_JSONDEF_FILE).toString();
         // LUBM Workload
-        WorkloadUtil.createLUBM_1_0_OriginalJSONDefFile(outputDir);
+        LUBMWorkloadUtil.createLUBM_1_0_OriginalJSONDefFile(outputDir);
         IGeospatialWorkLoadSpec lubm1_0WLS
                 = WorkLoadSpecUtil.deserializeFromJSON(outputFile);
         logger.info(lubm1_0WLS.serializeToJSON());
